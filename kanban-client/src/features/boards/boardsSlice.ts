f3eba7d494576b185d1bd06af12ec535b1a81805
Ihor Karychort <loppiror@gmail.com>
2025-05-21 13:55:38 +0300

    Update boardsSlice.ts

--- kanban-client/src/features/boards/boardsSlice.ts
+++ kanban-client/src/features/boards/boardsSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk, type PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Types
 export type Task = {
